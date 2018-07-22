3e6b7c46769d08fc4f1ee8c6e7e33b98ee60d1d0
simoneas02 <simoneas02@hotmail.com>
2018-07-22 20:42:52 -0300

    update Main component

--- src/Main.js
+++ src/Main.js
@@ -3,14 +3,17 @@ import React from 'react';
 import about from './assets/images/about.png'
 
 import './assets/style/main.css';
+import './assets/style/about.css';
 
 const Main = () => (
   <main className='main'>
-    <div className='main__content'>
-      <h2 className='main__title'>Sobre</h2>
-      <p className='main__text'>Uma iniciativa do Women Who Code Recife que tem o objetivo de desenvolver projetos pro-bonos e comerciais para colocar em prática os conhecimentos adquiridos nos meetups e grupos de estudos do WWCode. Oportunidade para aquisição de experiência profissional com atuação em projetos reais. Acreditamos que com a criação de um portfólio próprio os profissionais ganham mais visibilidade ,e aumentam sua competitividade no mercado.</p>
-    </div>
-    <img className='main__img' src={about} alt='' />
+    <section className='about'>
+      <div className='about__content'>
+        <h2 className='about__title'>Sobre</h2>
+        <p className='about__text'>Uma iniciativa do Women Who Code Recife que tem o objetivo de desenvolver projetos pro-bonos e comerciais para colocar em prática os conhecimentos adquiridos nos meetups e grupos de estudos do WWCode. Oportunidade para aquisição de experiência profissional com atuação em projetos reais. Acreditamos que com a criação de um portfólio próprio os profissionais ganham mais visibilidade ,e aumentam sua competitividade no mercado.</p>
+      </div>
+      <img className='about__img' src={about} alt='' />
+    </section>
   </main>
 );
 
