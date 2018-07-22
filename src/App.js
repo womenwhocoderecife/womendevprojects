28f55ed1c84070696c5907b852a8638a1bd694ab
simoneas02 <simoneas02@hotmail.com>
2018-07-22 18:36:58 -0300

    fix layout of website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
+import Main from './Main';
 import Footer from './Footer';
 
-import './assets/style/App.css';
+import './assets/style/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
         <Footer />
       </div>
     );
