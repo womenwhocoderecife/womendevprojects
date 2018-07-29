1a7310a08a3b3f9fde5a335fb510617d8b93e037
karinamachado <karinamachado@msn.com>
2018-07-29 00:31:53 -0300

    Issue Equipe

--- src/Main.js
+++ src/Main.js
@@ -4,10 +4,18 @@ import about from './assets/images/about.png';
 import group from './assets/icons/group.svg';
 import rocket from './assets/icons/rocket.svg';
 import tag from './assets/icons/tag.svg';
+import karina_perfil from './assets/images/karina_perfil.jpg';
+import simone from './assets/images/simone.jpg';
+import andreza from './assets/images/andreza.jpg';
+import will from './assets/images/willany.jpg';
+import github from './assets/icons/github.svg';
+import instagram from './assets/icons/instagram.svg';
+import twitter from './assets/icons/twitter.svg';
 
 import './assets/style/main/main.css';
 import './assets/style/main/about.css';
 import './assets/style/main/proposal.css';
+import './assets/style/main/team.css';
 
 const Main = () => (
   <main className="main">
@@ -93,6 +101,104 @@ const Main = () => (
         </div>
       </div>
     </section>
+    <section className="team">
+
+      <h2 className="main__title team__title">Equipe</h2>
+      <div className="team__content">
+        <div className="team__card">
+          <img
+            className="team__card__img"
+            src={karina_perfil}
+            alt="Karina"
+          />
+          <h3 className="team__card__title">Karina Machado</h3>
+          <p className="team__card__text">Front-End Developer</p>
+          <p className="team__card__text">WWcode Director, Recife.</p>
+         
+          <ul className='footer__list__social-network'>
+            <li className='footer__list__item__social-network'>
+              <a href='https://github.com/womenwhocoderecife'><img src={github} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://www.instagram.com/wwcoderecife/'><img src={instagram} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://twitter.com/WWCode_Recife'><img src={twitter} alt='logo'/></a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="team__card">
+          <img
+            className="team__card__img"
+            src={andreza}
+            alt="Andreza"
+          />
+          <h3 className="team__card__title">Andreza Leite</h3>
+          <p className="team__card__text">Docente UFRPE</p>
+          <p className="team__card__text">WWcode Director, Recife.</p>
+         
+          <ul className='footer__list__social-network'>
+            <li className='footer__list__item__social-network'>
+              <a href='https://github.com/womenwhocoderecife'><img src={github} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://www.instagram.com/wwcoderecife/'><img src={instagram} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://twitter.com/WWCode_Recife'><img src={twitter} alt='logo'/></a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="team__card">
+          <img
+            className="team__card__img"
+            src={simone}
+            alt="Simone"
+          />
+          <h3 className="team__card__title">Simone Amorim</h3>
+          <p className="team__card__text">Developer ThoughtWorks</p>
+          <p className="team__card__text">WWcode Lead, Recife.</p>
+         
+          <ul className='footer__list__social-network'>
+            <li className='footer__list__item__social-network'>
+              <a href='https://github.com/womenwhocoderecife'><img src={github} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://www.instagram.com/wwcoderecife/'><img src={instagram} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://twitter.com/WWCode_Recife'><img src={twitter} alt='logo'/></a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="team__card">
+          <img
+            className="team__card__img"
+            src={will}
+            alt="Willany"
+          />
+          <h3 className="team__card__title">Willany Silva</h3>
+          <p className="team__card__text">Developer ThoughtWorks</p>
+          <p className="team__card__text">WWcode Lead, Recife.</p>
+         
+          <ul className='footer__list__social-network'>
+            <li className='footer__list__item__social-network'>
+              <a href='https://github.com/womenwhocoderecife'><img src={github} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://www.instagram.com/wwcoderecife/'><img src={instagram} alt='logo'/></a>
+            </li>
+            <li className='footer__list__item__social-network'>
+              <a href='https://twitter.com/WWCode_Recife'><img src={twitter} alt='logo'/></a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </section>   
   </main>
 );
 
