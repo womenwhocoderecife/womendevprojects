296d7444328096495ead480502c42e370bea1418
karinamachado <karinamachado@msn.com>
2018-08-03 12:37:42 -0300

    Merge branch 'master' into karina
    # Conflicts:
    #	src/assets/style/main/statistic.css

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
+import '../assets/style/main/main.css';
+
 import About from './About';
 import Contact from './Contact';
 import Proposal from './Proposal';
 import Team from './Team';
 import Statistic from './Statistic';
-
-import '../assets/style/main/main.css';
 
 class Main extends Component {
   render() {
