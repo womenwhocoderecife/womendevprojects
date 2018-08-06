d3ca14343fb5a35ca00843f4d1644969d1ea43a5
simoneas02 <simoneas02@hotmail.com>
2018-08-05 23:51:26 -0300

    add validation for fields form

--- src/main/Form.js
+++ src/main/Form.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
 
+import FormValidator from './FormValidator';
+import validations from './validations';
+
 import '../assets/style/main/form.css';
 
 class Form extends Component {
   constructor(props) {
+
     super(props);
-    this.state = { value: '' };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.validator = new FormValidator(validations.login_rules);
+
+    this.state = {
+      email: '',
+      validation: this.validator.valid()
+    };
+
+    this.submitted = false;
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value.toUpperCase() });
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+  handleInputChange = event => {
     event.preventDefault();
+
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    const validation = this.validator.validate(this.state);
+    this.setState({ validation });
+    this.submitted = true;
+
+    if (validation.isValid) {
+      // handle actual form submission here
+    }
   }
 
   render() {
+
+    let validation = this.submitted ?
+      this.validator.validate(this.state) :
+      this.state.validation
+
     return (
       <form className="form">
         <h2 className="main__title form__title">
@@ -31,9 +58,9 @@ class Form extends Component {
             className="form__input"
             id="name"
             type="text"
+            required
             placeholder="seu nome"
-            value={this.state.value}
-            onChange={this.handleChange}
+            onChange={this.handleInputChange}
           />
           <div className="form__label-group">
             <label className="form__label" htmlFor="name">
@@ -48,6 +75,7 @@ class Form extends Component {
             className="form__input"
             id="email"
             type="email"
+            required
             placeholder="seu e-mail"
           />
           <div className="form__label-group">
@@ -63,6 +91,7 @@ class Form extends Component {
             className="form__input"
             id="subject"
             type="text"
+            required
             placeholder="título da mensagem"
           />
           <div className="form__label-group">
@@ -83,6 +112,7 @@ class Form extends Component {
           <textarea
             className="form__textarea"
             id="message"
+            required
             data-gramm="false"
           />
         </div>
@@ -91,6 +121,7 @@ class Form extends Component {
           id="submit"
           type="submit"
           value="Enviar"
+          onClick={this.handleFormSubmit}
         />
       </form>
     );
