6b248643e93b55acd14d58a5af385d637c7ec17f
simoneas02 <simoneas02@hotmail.com>
2018-08-17 19:44:49 -0300

    make menu become responsible for small display

--- src/Menu.js
+++ src/Menu.js
@@ -1,40 +1,95 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './assets/style/menu.css';
 import logo from './assets/images/logo.png';
 
-const Menu = () => (
-    <nav className="menu">
-        <a className="menu__logo" href="/">
-            <figure className="menu__figure">
-                <img className="menu__img" src={logo} alt="Logo Women Dev Projects" />
-            </figure>
-        </a>
+class Menu extends Component {
+  constructor() {
+    super();
+    this.state = { open: false };
 
-        <button aria-expanded="false" aria-controls="menu-list">
-            <span class="open">☰</span>
-            <span class="close">×</span>
-            Menu
-      </button>
-            
-        <ul className="menu__list">
+    this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  openMenu(event) {
+    event.preventDefault();
+    this.setState({ open: true });
+  }
+
+  closeMenu(event) {
+    event.preventDefault();
+    this.setState({ open: false });
+  }
+
+  render() {
+    // show menu
+    let showMenu;
+
+    if (this.state.open) {
+      showMenu = 'menu--show-menu';
+    } else {
+      showMenu = '';
+    }
+
+    return (
+      <div className="menu-container">
+        <button
+          className={`menu__button ${showMenu}`}
+          onClick={this.openMenu}
+        >
+          <span className="menu__button__icon">☰</span>
+          Menu
+        </button>
+        <button
+          className={`menu__button ${showMenu}`}
+          onClick={this.closeMenu}
+        >
+          <span className="menu__button__icon">×</span>
+          Menu
+        </button>
+        <nav className={`menu ${showMenu}`}>
+          <a className="menu__logo" href="/">
+            <figure className="menu__figure">
+              <img
+                className="menu__img"
+                src={logo}
+                alt="Logo Women Dev Projects"
+              />
+            </figure>
+          </a>
+
+          <ul className="menu__list">
             <li className="menu__list__item">
-                <a className="menu__list__link" href="#home">Home</a>
+              <a className="menu__list__link" href="#home">
+                Home
+              </a>
             </li>
             <li className="menu__list__item">
-                <a className="menu__list__link" href="#about">Sobre</a>
+              <a className="menu__list__link" href="#about">
+                Sobre
+              </a>
             </li>
             <li className="menu__list__item">
-                <a className="menu__list__link" href="#proposal">Nossa Proposta</a>
+              <a className="menu__list__link" href="#proposal">
+                Nossa Proposta
+              </a>
             </li>
             <li className="menu__list__item">
-                <a className="menu__list__link" href="#team">Equipe</a>
+              <a className="menu__list__link" href="#team">
+                Equipe
+              </a>
             </li>
             <li className="menu__list__item">
-                <a className="menu__list__link" href="#contact">Contato</a>
+              <a className="menu__list__link" href="#contact">
+                Contato
+              </a>
             </li>
-        </ul>
-    </nav>
-);
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default Menu;
