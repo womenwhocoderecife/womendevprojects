24ac1ab2bfa40bbeed099edb572baa1d63c60bd4
myllenaalves <myllenaalves971@gmail.com>
2019-04-02 13:58:55 -0300

    activate iterative counter

--- src/main/Statistic.js
+++ src/main/Statistic.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CountUp from 'react-countup';
 import calendar from '../assets/images/calendar.png';
 import check from '../assets/images/check.png';
 import coffee from '../assets/images/coffee.png';
@@ -15,25 +16,25 @@ const Statistic = () => (
           src={calendar}
           alt="calendar"
         />
-        <h4 className="statistic__tittle">15</h4>
+        <h4 className="statistic__tittle"><CountUp start={0} end={15} delay={2} duration={5}></CountUp></h4>
         <p className="statistic__text">Dias Trabalhados</p>
       </li>
 
       <li className="statistic__list__item">
         <img className="statistic__img" src={check} alt="check" />
-        <h4 className="statistic__tittle">2</h4>
+        <h4 className="statistic__tittle"><CountUp start={0} end={2} delay={2} duration={0}></CountUp></h4>
         <p className="statistic__text">Projetos Realizados</p>
       </li>
 
       <li className="statistic__list__item">
         <img className="statistic__img" src={coffee} alt="coffee" />
-        <h4 className="statistic__tittle">50</h4>
+        <h4 className="statistic__tittle"><CountUp start={0} end={50} delay={2} duration={10}></CountUp></h4>
         <p className="statistic__text">Cafés</p>
       </li>
 
       <li className="statistic__list__item">
         <img className="statistic__img" src={heart} alt="heart" />
-        <h4 className="statistic__tittle">2</h4>
+        <h4 className="statistic__tittle"><CountUp start={0} end={2} delay={2} duration={0}></CountUp></h4>
         <p className="statistic__text">Clientes Satisfeitos</p>
       </li>
     </ul>
