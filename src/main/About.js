e4c033150071e7fb9fc0b062f576130db198248b
Karina Machado <karinamachado@MacBook-Pro-de-Karina.local>
2019-04-06 12:01:55 -0300

    ScrollAnimation

--- src/main/About.js
+++ src/main/About.js
@@ -3,23 +3,31 @@ import React from 'react';
 // import about from '../assets/images/hands.png';
 import about from '../assets/images/about2.jpg';
 import '../assets/style/main/about.css';
+import ScrollAnimation from 'react-animate-on-scroll'
+import "animate.css/animate.min.css";
 
 const About = () => (
   <section className="about" id="about">
-    <div className="about__content">
+    <div className="about__content animated fadeIn">
+    <ScrollAnimation animateIn='fadeIn' animateOut='fadeOut'>
       <h2 className="main__title about__title">WomenDev Projects</h2>
-      <p className="about__text">
-        Uma iniciativa do Women Who Code Recife que tem o objetivo de
-        desenvolver projetos pro-bonos e comerciais para colocar em
-        prática os conhecimentos adquiridos nos meetups e grupos de
-        estudos do WWCode. Oportunidade para aquisição de experiência
-        profissional com atuação em projetos reais. Acreditamos que
-        com a criação de um portfólio próprio os profissionais ganham
-        mais visibilidade ,e aumentam sua competitividade no mercado.
-      </p>
+      </ScrollAnimation>
+      <ScrollAnimation animateIn="fadeInUp">
+        <p className="about__text">
+          Uma iniciativa do Women Who Code Recife que tem o objetivo de
+          desenvolver projetos pro-bonos e comerciais para colocar em
+          prática os conhecimentos adquiridos nos meetups e grupos de
+          estudos do WWCode. Oportunidade para aquisição de experiência
+          profissional com atuação em projetos reais. Acreditamos que
+          com a criação de um portfólio próprio os profissionais ganham
+          mais visibilidade ,e aumentam sua competitividade no mercado.
+        </p>
+      </ScrollAnimation>
     </div>
     <figure className="about__figure">
+    <ScrollAnimation animateIn="fadeIn">
       <img className="about__img" src={about} alt="" />
+      </ScrollAnimation>
     </figure>
   </section>
 );
--- src/main/Projects.js
+++ src/main/Projects.js
@@ -3,6 +3,9 @@ import '../assets/style/main/Projects.css'
 import ImageProBono from '../assets/images/projects.jpeg'
 import ImageComercial from '../assets/images/projects2.jpg'
 import ImageParticipate from '../assets/images/projects3.jpg'
+import {Animated} from "react-animated-css"
+import ScrollAnimation from 'react-animate-on-scroll'
+import "animate.css/animate.min.css";
 
 class Projects extends Component{
     render(){
@@ -11,23 +14,32 @@ class Projects extends Component{
         <div>
             <div className='service-section-one'>
                 <div>
+                <ScrollAnimation animateIn='bounceInLeft'>         
                     <img src={ImageProBono} alt='projetos pro-bonos' />
+                </ScrollAnimation> 
                 </div>
 
                 <div>
+                <ScrollAnimation animateIn='fadeIn'>
                     <h1>Projetos Pro-Bonos</h1>
+                </ScrollAnimation>     
+                    <ScrollAnimation animateIn="fadeInUp">
                         <p>
                         Projetos Web/Mobile para organizações sem fins lucrativos.<br></br>
                         Excelente oportunidade para os membros que<br></br> estão começando na área de tecnologia.<br></br>
                         Ajuda as organizações/iniciativas na divulgação de suas<br></br> ações/atividades/campanhas.<br></br>
                         Apoio da comunidade para desenvolver <br></br>habilidades profissionais.
                         </p>
+                    </ScrollAnimation>    
                 </div>
             </div>
 
             <div className='service-section-two'>
                 <div>
+                <ScrollAnimation animateIn='fadeIn'>
                     <h1>Projetos Comerciais</h1>
+                </ScrollAnimation>       
+                    <ScrollAnimation animateIn="fadeInUp">
                     <p>
                     Projetos Web/Mobile para empresas e organizações.<br></br>
                     Oportunidade para os membros participarem de projetos reais por meio da prestação de serviços.<br></br>  
@@ -37,25 +49,36 @@ class Projects extends Component{
                     
                     <br></br>
                     </p>
+                    </ScrollAnimation>
                 </div>
                 <div>
-                    <img src={ImageComercial} alt='projetos comerciais' />
+                <ScrollAnimation animateIn="fadeIn">
+                    <img src={ImageComercial} alt='projetos comerciais' /> 
+                    </ScrollAnimation>   
                 </div>
+                
             </div>
 
 
             <div className='service-section-one'>
                 <div>
+                <ScrollAnimation animateIn='bounceInLeft'
+  animateOut='bounceOutLeft'>
                     <img src={ImageParticipate} alt='projetos pro-bonos' />
+                    </ScrollAnimation>   
                 </div>
 
                 <div>
+                <ScrollAnimation animateIn='fadeIn'>
                     <h1>Como Posso Participar?</h1>
+                </ScrollAnimation>   
+                    <ScrollAnimation animateIn="fadeInUp">
                         <p>
                         Membros do WWcode Recife participando de<br></br> grupos de estudos e meetups práticos.<br></br>
                         Líderes/Voluntários do WWCode apoiando com<br></br> mentoria e liderando times.<br></br>
                         Buscamos pessoas comprometidas, éticas, disponíveis,<br></br> que gostem de aprender, de compartilharconhecimento <br></br> e de trabalhar em equipe.
                         </p>
+                    </ScrollAnimation>   
                 </div>
             </div>
 
