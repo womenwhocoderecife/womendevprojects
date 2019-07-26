51b1aa1183d8d7302038c905eea1e0c13b5afd0c
willanysilva <willany_lp@hotmail.com>
2019-07-25 22:14:16 -0300

    Fix #21 Refactoring App component

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Header from '../Header';
 import Main from '../main/Main';
 import Footer from '../Footer';
 
-import './index.css';
+import { Container } from './styles';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app" id="home">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Container>
+      <Header />
+      <Main />
+      <Footer />
+    </Container>
+  );
 }
 
 export default App;
